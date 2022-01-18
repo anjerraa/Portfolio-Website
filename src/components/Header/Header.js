3ff30f71312f9b47c0e13d54186fb4c7eb1a3458
Mai Pham <angellapham240@gmail.com>
2022-01-18 20:03:18 +1100

    Slightly modified the header text and icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,29 @@
+// Allow us to link different pages or sections in our own website.
 import Link from 'next/link';
 import React from 'react';
+// Allow us to use icon presets.
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
-
+import { AiOutlineCode } from 'react-icons/ai';
+// Premade header styles.
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+    <Div1>
+      <Link href="/">
+        <a style={{display: "flex", alignItems: "center", color: "white"}}>
+          <AiOutlineCode size="3rem"/> <span>[Mai] Portfolio</span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          
+        </Link>
+      </li>
+    </Div2>
+  </Container>
 );
 
 export default Header;
