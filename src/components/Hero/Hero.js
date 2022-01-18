959631d8343fc4ad9719ac3531d4996e4c28bdaf
Mai Pham <angellapham240@gmail.com>
2022-01-18 23:21:19 +1100

    Added a simple welcome message to the front page

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,22 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br/>
+        [Mai] Personal Portfolio!
+      </SectionTitle>
+      <SectionText>
+        Hi there! I am a second year Software Engineering student at the 
+        University of New South Wales (Sydney, Australia). I have experience 
+        with Python, C and MIPS Assembly. I am currently learning Javascript 
+        through a variety of personal projects, such as making a todo list, 
+        Discord bot and this portfolio website!
+      </SectionText>
+      <Button onClick={() => window.location = "#projects"}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
