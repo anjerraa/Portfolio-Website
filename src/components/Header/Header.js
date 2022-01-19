9670d01b9b7644afbf9326d2435d074f18859ca8
Mai Pham <angellapham240@gmail.com>
2022-01-19 16:13:21 +1100

    Added a social icon that is linked to Youtube

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@
 import Link from 'next/link';
 import React from 'react';
 // Allow us to use icon presets.
-import { AiOutlineCode, AiFillGithub, AiFillLinkedin, AiOutlineMail } from 'react-icons/ai';
+import { AiOutlineCode, AiFillGithub, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai';
 import { MdMail } from 'react-icons/md';
 // Premade header styles.
 import { Container, Span, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
@@ -45,6 +45,9 @@ const Header = () =>  (
       </SocialIcons>
       <SocialIcons href="https://www.linkedin.com/in/mai-ngoc-pham/">
         <AiFillLinkedin size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href="https://www.youtube.com/c/Anjerraa">
+        <AiFillYoutube size="3.3rem"/>
       </SocialIcons>
       <SocialIcons href="mailto:angellapham240@gmail.com">
         <MdMail size="3.3rem"/>
